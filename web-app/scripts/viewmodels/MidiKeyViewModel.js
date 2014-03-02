174939a2d6eccf2784be4037210646056221a055
Riley Guerin <guerin.riley@gmail.com>
2014-03-01 20:04:44 -0800

    make keys selectable

--- web-app/scripts/viewmodels/MidiKeyViewModel.js
+++ web-app/scripts/viewmodels/MidiKeyViewModel.js
@@ -1,12 +1,20 @@
 define([
     'underscore',
+    'jquery',
     'knockout'
-], function (_, ko) {
+], function (_, $, ko) {
     'use strict';
     return function (socket, key) {
         this.name = key.name
         this._id = key._id
         this.identifier = key.identifier
+        this.active = ko.observable(false)
+
+        this.toggleActive = function (key, event) {
+            var $currentTarget = $(event.currentTarget)
+            $currentTarget.siblings('.active').removeClass('active')
+            $currentTarget.addClass('active')
+        }
 
         this.remove = function () {
             socket.emit('removeKey', key._id)
